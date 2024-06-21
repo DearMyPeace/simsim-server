652503696ccba1e1bcf4b824c54e186d23cb3e1a
YunjooCho <73283078+YunjooCho@users.noreply.github.com>
2024-06-21 21:48:53 +0900

    [Feat] Setting Swagger

---
+++ src/main/java/com/project/simsim_server/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.project.simsim_server.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+
+@Configurable
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Spring Boot REST API Specifications")
+                .description("Specification")
+                .version("1.0.0");
+    }
+}
