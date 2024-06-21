623f4bf3098462f38ffd50e12052ed953405bae5
YunjooCho <73283078+YunjooCho@users.noreply.github.com>
2024-06-21 21:26:46 +0900

    [Refact] Refactoring AI Tagle DDL

--- src/main/sql/ddl.sql
+++ src/main/sql/ddl.sql
@@ -17,7 +17,7 @@ create table daily_ai_response_tbl (
     ai_target_date date not null,
     ai_reply_summary varchar(30),
     ai_reply_content varchar(500),
-    ai_analyze_emotions varchar(100),
+    ai_analyze_emotions varchar(20),
     ai_analyze_factors varchar(300),
     created_date datetime(6) not null,
     modified_date datetime(6) not null,
