f633a02798a3a9c19a470be580fad2d7363aa0f1
YunjooCho <73283078+YunjooCho@users.noreply.github.com>
2024-06-22 22:13:24 +0900

    [Feat] Implemente the Google Login with OAuth2

---
+++ src/main/java/com/project/simsim_server/repository/user/UsersRepository.java
@@ -0,0 +1,10 @@
+package com.project.simsim_server.repository.user;
+
+import com.project.simsim_server.domain.user.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsersRepository extends JpaRepository<Users, Long> {
+    Optional<Users> findByEmail(String email);
+}
--- src/main/sql/ddl.sql
+++ src/main/sql/ddl.sql
@@ -1,3 +1,19 @@
+drop table if exists users_tbl;
+create table users_tbl (
+    user_id bigint not null auto_increment,
+    user_email varchar(30) not null,
+    user_name varchar(10) not null,
+    user_role enum ('ADMIN','GUEST','USER') not null,
+    user_grade tinyint default 0 not null,
+    user_piece_cnt integer default 0 not null,
+    user_persona char(1) default 'P' not null,
+    user_bg_image varchar(300),
+    user_status char(1) default 'Y' not null,
+    created_date datetime(6) not null,
+    modified_date datetime(6) not null,
+    primary key (user_id)
+);
+
 drop table if exists diary_tbl;
 create table diary_tbl (
     diary_id bigint not null auto_increment,
