8bdc2388d7e3ca6f96a4d5b297903b9f6edd585a
YunjooCho <73283078+YunjooCho@users.noreply.github.com>
2024-07-03 20:33:36 +0900

    [Feat] Implement the API for generating AI Letters

--- src/main/java/com/project/simsim_server/domain/user/Reply.java
+++ src/main/java/com/project/simsim_server/domain/user/Reply.java
@@ -6,8 +6,9 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum Reply {
-    EMPTY("N", "수령할 편지 없음"),
-    OCCUPIED("Y", "수령할 편지 있음");
+    DEFAULT("D", "기본"),
+    CHECK("C", "편지 안읽음"),
+    RECEIVE("R", "편지 읽음");
 
     private final String key;
     private final String title;
--- src/main/java/com/project/simsim_server/exception/ai/AIErrorCode.java
+++ src/main/java/com/project/simsim_server/exception/ai/AIErrorCode.java
@@ -5,7 +5,9 @@ import com.project.simsim_server.exception.ErrorType;
 public enum AIErrorCode implements ErrorType {
 
     AILETTERS_NOT_FOUND("AI 편지가 존재하지 않습니다.", 400),
-    AI_MAIL_FAIL("AI 편지 생성이 실패했습니다.", 404);
+    AI_MAIL_FAIL("AI 편지 생성이 실패했습니다.", 404),
+    AI_NOT_INVALID_DATE("AI를 생성할 수 없는 날짜입니다.", 400),
+    NOT_MEET_USER_GRADE("현재 등급으로 조회할 수 없습니다. 내일 다시 시도해주세요.", 403);
 
     private final String message;
     private final int code;
