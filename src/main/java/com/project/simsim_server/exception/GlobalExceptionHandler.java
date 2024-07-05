475d8d6850980916ffbf53f6f69938b7318e66e7
YunjooCho <73283078+YunjooCho@users.noreply.github.com>
2024-07-05 15:34:26 +0900

    Update GlobalExceptionHandler.java

--- src/main/java/com/project/simsim_server/exception/GlobalExceptionHandler.java
+++ src/main/java/com/project/simsim_server/exception/GlobalExceptionHandler.java
@@ -4,7 +4,6 @@ import com.project.simsim_server.exception.ai.AIException;
 import com.project.simsim_server.exception.auth.OAuthException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -27,19 +26,19 @@ public class GlobalExceptionHandler {
     public ResponseEntity<Object> handleOAuthException(OAuthException ex) {
         ErrorType errorType = ex.getErrorType();
         String errorMessage = errorType.getMessage();
-        String errorCode = String.valueOf(errorType.getCode());
+        int errorCode = errorType.getCode();
         HttpStatus status = HttpStatus.valueOf(errorCode);
 
-        return new ResponseEntity<>(new ErrorResponse(errorMessage, errorCode), status);
+        return new ResponseEntity<>(new ErrorResponse(errorMessage, String.valueOf(errorCode)), status);
     }
 
     @ExceptionHandler(AIException.class)
     public ResponseEntity<Object> handleAIException(AIException ex) {
         ErrorType errorType = ex.getErrorType();
         String errorMessage = errorType.getMessage();
-        String errorCode = String.valueOf(errorType.getCode());
+        int errorCode = errorType.getCode();
         HttpStatus status = HttpStatus.valueOf(errorCode);
 
-        return new ResponseEntity<>(new ErrorResponse(errorMessage, errorCode), status);
+        return new ResponseEntity<>(new ErrorResponse(errorMessage, String.valueOf(errorCode)), status);
     }
 }
