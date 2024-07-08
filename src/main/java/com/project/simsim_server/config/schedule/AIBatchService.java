22102ed19adcbc4dcc0e4ec6bb036b4a9d384013
YunjooCho <73283078+YunjooCho@users.noreply.github.com>
2024-07-08 20:47:19 +0900

    [Feat] Delete First Reply Batch Schedule

--- src/main/java/com/project/simsim_server/config/schedule/AIBatchService.java
+++ src/main/java/com/project/simsim_server/config/schedule/AIBatchService.java
@@ -1,49 +1,35 @@
 package com.project.simsim_server.config.schedule;
 
-//@Slf4j
-//@RequiredArgsConstructor
-//@Service
-//public class AIBatchService {
-//
-//    private AIService aiService;
-//    private UsersRepository usersRepository;
-//
-//    @Transactional
-//    public void saveAuto() {
-//        /**
-//         * 모든 회원의 전날 일기에 대한 AI 응답 생성 로직
-//         */
-//        // 모든 회원 조회
-////        List<Users> userList = usersRepository.findAllAndUserStatus();
-//        // 전날 일기 목록 조회
-////        LocalDate targetDate = LocalDate.now().minusDays(1);
-////        LocalDateTime startDateTime = LocalDate.now().minusDays(1).atStartOfDay();
-////        LocalDateTime endDateTime = LocalDate.now().minusDays(1).atTime(LocalTime.MAX);
-//
-////        for (Users user : userList) {
-////            try {
-////                processUser(user, targetDate, startDateTime, endDateTime);
-////            } catch (Exception e) {
-////                log.error("---[SimSimSchedule] 에러 처리 userId = {}", user.getUserId(), e);
-////            }
-////        }
-//
-//        /**
-//         * 테스트용 로직 : 5번 유저의 6월치 분석 배치
-//         */
-//        Long testId = 5L;
-//        Optional<Users> usersOptional = usersRepository.findById(testId);
-//        Users user = usersOptional.get();
-//        LocalDate targetDate = LocalDate.of(2024, 6, 1);
-//        for (int i = 1; i <= 30; i++){
-//            LocalDateTime startDateTime = targetDate.atStartOfDay();
-//            LocalDateTime endDateTime = targetDate.atTime(LocalTime.MAX);
-//            try {
-//                aiService.processUser(user, targetDate, startDateTime, endDateTime);
-//                targetDate = targetDate.plusDays(1);
-//            } catch (Exception e) {
-//                log.error("---[SimSimSchedule] 에러 처리 userId = {}", user.getUserId(), e);
-//            }
-//        }
-//    }
-//}
+import com.project.simsim_server.domain.ai.DailyAiInfo;
+import com.project.simsim_server.repository.ai.DailyAiInfoRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class AIBatchService {
+
+    private final DailyAiInfoRepository dailyAiInfoRepository;
+
+    @Transactional
+    public void deleteFirstReply() {
+        log.info("--[SimSim Schedule] 안내 메일 DB 삭제 시작");
+
+        LocalDate standardDay = LocalDate.now().minusDays(2);
+        List<DailyAiInfo> firstReplies = dailyAiInfoRepository.findByFirstReply();
+        for (DailyAiInfo info : firstReplies) {
+            if (info.getTargetDate().isBefore(standardDay)) {
+                log.info("--[SimSim Schedule] 삭제 aiId : {}, targetDate : {}", info.getAiId(), info.getTargetDate());
+                dailyAiInfoRepository.deleteById(info.getAiId());
+
+            }
+        }
+        log.info("--[SimSim Schedule] 안내 메일 DB 삭제 종료");
+    }
+}
