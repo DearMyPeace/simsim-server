b653dc5f1c7fc8c0a5064119daf9a98a81e326cd
YunjooCho <73283078+YunjooCho@users.noreply.github.com>
2024-07-08 22:55:33 +0900

    Update DatabaseConverter.java

--- src/main/java/com/project/simsim_server/config/encrytion/DatabaseConverter.java
+++ src/main/java/com/project/simsim_server/config/encrytion/DatabaseConverter.java
@@ -15,27 +15,48 @@ public class DatabaseConverter implements AttributeConverter<String, String> {
     private final EncryptionUtil encryptionUtil;
     private final Pattern base64Pattern = Pattern.compile("^[A-Za-z0-9+/]+={0,2}$");
 
+//    @Override
+//    public String convertToDatabaseColumn(String attribute) {
+//        try {
+//            return encryptionUtil.encrypt(attribute);
+//        } catch (Exception e) {
+//            throw new RuntimeException("Could not encrypt attribute", e);
+//        }
+//    }
+//
+//    @Override
+//    public String convertToEntityAttribute(String dbData) {
+//        try {
+//            // 데이터가 Base64 패턴에 맞는지 확인하여 암호화 여부 판단
+//            if (dbData != null && base64Pattern.matcher(dbData).matches() && dbData.length() > 12) {
+//                return encryptionUtil.decrypt(dbData);
+//            } else {
+//                // 암호화되지 않은 데이터는 그대로 반환
+//                return dbData;
+//            }
+//        } catch (Exception e) {
+//            throw new RuntimeException("Could not decrypt database column", e);
+//        }
+//    }
+
+
+    //application --> database
     @Override
     public String convertToDatabaseColumn(String attribute) {
         try {
             return encryptionUtil.encrypt(attribute);
         } catch (Exception e) {
-            throw new RuntimeException("Could not encrypt attribute", e);
+            throw new RuntimeException(e);
         }
     }
 
+    //database --> application
     @Override
     public String convertToEntityAttribute(String dbData) {
         try {
-            // 데이터가 Base64 패턴에 맞는지 확인하여 암호화 여부 판단
-            if (dbData != null && base64Pattern.matcher(dbData).matches() && dbData.length() > 12) {
-                return encryptionUtil.decrypt(dbData);
-            } else {
-                // 암호화되지 않은 데이터는 그대로 반환
-                return dbData;
-            }
+            return encryptionUtil.decrypt(dbData);
         } catch (Exception e) {
-            throw new RuntimeException("Could not decrypt database column", e);
+            throw new RuntimeException(e);
         }
     }
 }
