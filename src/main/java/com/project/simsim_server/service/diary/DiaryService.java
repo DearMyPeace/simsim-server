51ee60b408175f37b366a6edc25336049b991052
YunjooCho <73283078+YunjooCho@users.noreply.github.com>
2024-07-10 02:08:11 +0900

    Update DiaryService.java

--- src/main/java/com/project/simsim_server/service/diary/DiaryService.java
+++ src/main/java/com/project/simsim_server/service/diary/DiaryService.java
@@ -72,7 +72,8 @@ public class DiaryService {
     public DiaryResponseDTO save(DiaryRequestDTO diaryRequestDTO, Long userId) {
         LocalDate targetDate = LocalDate.from(diaryRequestDTO.getCreatedDate().plusHours(9));
 
-        log.warn("조회 날짜 : {}", diaryRequestDTO.getCreatedDate());
+        log.warn("들어온 날짜 : {}", diaryRequestDTO.getCreatedDate());
+        log.warn("조회 날짜 : {}", diaryRequestDTO.getCreatedDate().plusHours(9));
 
         List<Diary> todayDiaries
                 = diaryRepository.findByCreatedAtAndUserId(userId, targetDate);
