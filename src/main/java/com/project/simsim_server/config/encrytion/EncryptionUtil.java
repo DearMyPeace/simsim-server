21424cfad5d00a5255aff6c88eb0c7a9f88896ee
InryeolChoi <dlsfuf0316@gmail.com>
2024-10-17 16:40:24 +0900

    rebase

--- src/main/java/com/project/simsim_server/config/encrytion/EncryptionUtil.java
+++ src/main/java/com/project/simsim_server/config/encrytion/EncryptionUtil.java
@@ -23,13 +23,13 @@ public class EncryptionUtil {
     // 사전 확인용 메소드
     @PostConstruct
     public void init() {
-        log.warn("Key 값: {}", this.key);
+        log.warn("Key 값: {}", key);
     }
 
     //encode
     public String encrypt(String valueToEnc) throws Exception {
-        Key key = generateKey(1);
-        log.warn("encrypt 상태: {}", this.key);
+        Key key = generateKey();
+        log.warn("encrypt 상태: {}", key);
         Cipher c = Cipher.getInstance(ALGORITHM);
         c.init(Cipher.ENCRYPT_MODE, key);
         byte[] encValue = c.doFinal(valueToEnc.getBytes());
@@ -39,8 +39,8 @@ public class EncryptionUtil {
 
     //decode
     public String decrypt(String encryptedValue) throws Exception {
-        Key key = generateKey(2);
-        log.warn("decrypt 상태: {}", this.key);
+        Key key = generateKey();
+        log.warn("decrypt 상태: {}", key);
         Cipher c = Cipher.getInstance(ALGORITHM);
         c.init(Cipher.DECRYPT_MODE, key);
         byte[] decodedValue = Base64.getDecoder().decode(encryptedValue);
@@ -49,8 +49,7 @@ public class EncryptionUtil {
         return new String(decValue);
     }
 
-    private Key generateKey(int num) {
-        log.warn("{}, generateKey()의 this.key 상태 = {}", num, this.key);
+    private Key generateKey() {
         return new SecretKeySpec(key.getBytes(), ALGORITHM);
     }
 
