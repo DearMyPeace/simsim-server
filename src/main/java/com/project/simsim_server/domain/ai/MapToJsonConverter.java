d38c7e6c81caf94295047888292e61c83b3d2db3
YunjooCho <73283078+YunjooCho@users.noreply.github.com>
2024-10-29 21:55:57 +0900

    Update MapToJsonConverter.java

--- src/main/java/com/project/simsim_server/domain/ai/MapToJsonConverter.java
+++ src/main/java/com/project/simsim_server/domain/ai/MapToJsonConverter.java
@@ -9,6 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 @Slf4j
@@ -21,18 +22,18 @@ public class MapToJsonConverter implements AttributeConverter<Map<String, Double
     @Override
     public String convertToDatabaseColumn(Map<String, Double> attribute) {
         try {
-            return objectMapper.writeValueAsString(attribute);
+            return attribute == null ? null : objectMapper.writeValueAsString(attribute);
         } catch (JsonProcessingException e) {
-            throw new RuntimeException("--- [SimSimInfo] JSON 직렬화 에러 ---", e);
+            throw new RuntimeException("--- [SimSimInfo] JSON 직렬화 에러", e);
         }
     }
 
     @Override
     public Map<String, Double> convertToEntityAttribute(String dbData) {
         try {
-            return objectMapper.readValue(dbData, Map.class);
+            return dbData == null ? new HashMap<>() : objectMapper.readValue(dbData, Map.class);
         } catch (IOException e) {
-            throw new RuntimeException("--- [SimSimInfo] JSON 역직렬화 에러 ---", e);
+            throw new RuntimeException(" [SimSimInfo] JSON 역직렬화 에러", e);
         }
     }
 }
