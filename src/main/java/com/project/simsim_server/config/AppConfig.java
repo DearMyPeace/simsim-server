6b406870e791abe96486a93a9fd381e821580fbb
YunjooCho <73283078+YunjooCho@users.noreply.github.com>
2024-11-14 21:07:27 +0900

    [Feat] Modified RestTemplate builder

--- src/main/java/com/project/simsim_server/config/AppConfig.java
+++ src/main/java/com/project/simsim_server/config/AppConfig.java
@@ -1,5 +1,6 @@
 package com.project.simsim_server.config;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -8,7 +9,9 @@ import org.springframework.web.client.RestTemplate;
 public class AppConfig {
 
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder
+                .additionalInterceptors(new LoggingInterceptor())
+                .build();
     }
 }
