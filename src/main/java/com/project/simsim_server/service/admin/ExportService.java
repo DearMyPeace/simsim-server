a537cfb4390711531072e77f8b69101ad0ed7df8
YunjooCho <73283078+YunjooCho@users.noreply.github.com>
2024-11-28 18:38:12 +0900

    [Feat] Removed the file download logic from the server

--- src/main/java/com/project/simsim_server/service/admin/ExportService.java
+++ src/main/java/com/project/simsim_server/service/admin/ExportService.java
@@ -8,16 +8,12 @@ import com.project.simsim_server.repository.ai.DailyAiInfoRepository;
 import com.project.simsim_server.repository.diary.DiaryRepository;
 import com.project.simsim_server.repository.user.UsersRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.*;
 import java.util.stream.Collectors;
 
-@Slf4j
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 @Service
@@ -34,25 +30,6 @@ public class ExportService {
         }
 
         List<Diary> allDiaries = diaryRepository.findAll();
-        try (FileWriter writer = new FileWriter(fileName)) {
-            writer.append("diary_id,user_id,diary_content,diary_list_key,diary_delete_yn,marked_date,created_date,modified_date,is_send_able\n");
-            for (Diary diary : allDiaries) {
-                writer.append(String.format(
-                        "%d,%d,\"%s\",%s,%s,%s,%s,%s,%s\n",
-                        diary.getDiaryId(),
-                        diary.getUserId(),
-                        diary.getContent(),
-                        diary.getListKey(),
-                        diary.getDiaryDeleteYn(),
-                        diary.getMarkedDate(),
-                        diary.getCreatedDate(),
-                        diary.getModifiedDate(),
-                        diary.getSendAble()
-                ));
-            }
-        } catch (IOException e) {
-            log.error("--- [SimSimError] 기록 CSV 파일 생성 실패: {}", fileName, e);
-        }
 
         return allDiaries.stream().map((diary -> {
             Map<String, Object> diaryMap = new LinkedHashMap<>();
@@ -76,24 +53,6 @@ public class ExportService {
         }
 
         List<DailyAiInfo> responses = dailyAiInfoRepository.findAll();
-        try (FileWriter writer = new FileWriter(fileName)) {
-            writer.append("ai_id,user_id,ai_target_date,ai_diary_summary,ai_reply_content,ai_reply_status,created_date,modified_date\n");
-            for (DailyAiInfo reponse : responses) {
-                writer.append(String.format(
-                        "%d,%d,%s,\"%s\",\"%s\",%s,%s,%s\n",
-                        reponse.getAiId(),
-                        reponse.getUserId(),
-                        reponse.getTargetDate(),
-                        reponse.getDiarySummary().replace("\"", "\"\""),
-                        reponse.getReplyContent().replace("\"", "\"\""),
-                        reponse.getReplyStatus(),
-                        reponse.getCreatedDate(),
-                        reponse.getModifiedDate()
-                ));
-            }
-        } catch (IOException e) {
-            log.error("--- [SimSimError] 편지 CSV 파일 생성 실패: {}", fileName, e);
-        }
 
         return responses.stream().map((response) -> {
             Map<String, Object> responseMap = new LinkedHashMap<>();
