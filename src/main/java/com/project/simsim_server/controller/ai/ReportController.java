c8286fe92d8a98f08b8719bfe5e6581b0b8a1a7c
YunjooCho <73283078+YunjooCho@users.noreply.github.com>
2025-03-10 18:28:23 +0900

    [Refact] Delete Emotions API

--- src/main/java/com/project/simsim_server/controller/ai/ReportController.java
+++ src/main/java/com/project/simsim_server/controller/ai/ReportController.java
@@ -1,12 +1,10 @@
 package com.project.simsim_server.controller.ai;
 
 import com.project.simsim_server.config.auth.jwt.AuthenticationService;
-import com.project.simsim_server.dto.ai.client.WeekEmotionsResponseDTO;
-import com.project.simsim_server.dto.ai.client.WeekSummaryResponseDTO;
+
 import com.project.simsim_server.service.report.ReportService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -19,16 +17,4 @@ public class ReportController {
 
     private final ReportService reportService;
     private final AuthenticationService authenticationService;
-
-    @GetMapping("/week")
-    public WeekEmotionsResponseDTO getWeeksReportEmotions(@RequestParam("targetDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate targetDate) {
-        Long userId = authenticationService.getUserIdFromAuthentication();
-        return reportService.weekReportEmotions(userId, targetDate);
-    }
-
-    @GetMapping("/week/{targetDate}")
-    public WeekSummaryResponseDTO getWeeksReportSummary(@PathVariable("targetDate") LocalDate targetDate) {
-        Long userId = authenticationService.getUserIdFromAuthentication();
-        return reportService.weekReportSummary(userId, targetDate);
-    }
 }
--- src/main/java/com/project/simsim_server/service/ai/AIService.java
+++ src/main/java/com/project/simsim_server/service/ai/AIService.java
@@ -3,11 +3,14 @@ package com.project.simsim_server.service.ai;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.project.simsim_server.domain.ai.DailyAiInfo;
+import com.project.simsim_server.domain.ai.MonthlyReport;
 import com.project.simsim_server.domain.diary.Diary;
 import com.project.simsim_server.domain.user.Users;
+import com.project.simsim_server.dto.ai.client.AILetterResponseDTO;
 import com.project.simsim_server.dto.ai.fastapi.*;
 import com.project.simsim_server.exception.ai.AIException;
 import com.project.simsim_server.repository.ai.DailyAiInfoRepository;
+import com.project.simsim_server.repository.ai.MonthlyReportRepository;
 import com.project.simsim_server.repository.diary.DiaryRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -20,6 +23,7 @@ import org.springframework.web.client.RestTemplate;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -35,30 +39,23 @@ public class AIService {
 
     private final ObjectMapper objectMapper;
     private final RestTemplate restTemplate;
-    private final DailyAiInfoRepository dailyAiInfoRepository;
     private final DiaryRepository diaryRepository;
+    private final DailyAiInfoRepository dailyAiInfoRepository;
+    private final MonthlyReportRepository monthlyReportRepository;
     private final String AI_LETTER_URL = "http://127.0.0.1:8000/ai/v1/letter";
-    private final String AI_EMOTION_URL = "http://127.0.0.1:8000/ai/v1/emotion";
     private final String AI_SUMMARY_URL = "http://127.0.0.1:8000/ai/v1/summary";
+    private final String AI_KEYWORDS_URL = "http://127.0.0.1:8000/ai/v1/keywords";
 
 
     /**
      * 1. AI 요청용 DTO 생성
-     * @param user
-     * @param targetDate
-     * @param targetDiaries
-     * @return
      */
     public DailyAiLetterRequestDTO generateRequestData(Users user, LocalDate targetDate, List<Diary> targetDiaries) {
         // 페르소나 정보
         String persona = user.getPersona();
         log.warn("----[분석할 페르소나] {} : {}", user.getEmail(), user.getPersona());
 
-        // 14일~2일전 일기 요약 정보
-        LocalDate startDate = targetDate.minusDays(14);
-        LocalDate endDate = targetDate.minusDays(2);
-        List<DailyAiInfo> summaryList = dailyAiInfoRepository.findAllByIdAndTargetDate(user.getUserId(), startDate, endDate);
-
+        // 요청 일자 하루치의 기록(AI 편지 생성용)
         List<DiaryContentDTO> diaries = new ArrayList<>();
         for (Diary diary : targetDiaries) {
             diaries.add(DiaryContentDTO.builder()
@@ -67,22 +64,36 @@ public class AIService {
                     .build());
         }
 
-        // 감정 정보
+        // 2주간 AI 응답 정보(14일 ~ 2일전)
+        LocalDate startDate = targetDate.minusDays(14);
+        LocalDate endDate = targetDate.minusDays(2);
+        List<DailyAiInfo> summaryList = dailyAiInfoRepository.findAllByIdAndTargetDate(user.getUserId(), startDate, endDate);
+
         List<DiarySummaryDTO> summaries= new ArrayList<>();
         for (DailyAiInfo info : summaryList) {
             summaries.add(DiarySummaryDTO.builder()
                     .date(info.getTargetDate())
                     .content(info.getReplyContent())
-                    .positive(convertStringToList(info.getAnalyzePositive()))
-                    .neutral(convertStringToList(info.getAnalyzeNeutral()))
-                    .negative(convertStringToList(info.getAnalyzeNegative()))
+                    .build());
+        }
+
+        // 월별 기록 정보(키워드 정보 생성용)
+        LocalDateTime startDateTime = targetDate.withDayOfMonth(1).atStartOfDay();
+        LocalDateTime endDateTime = targetDate.atTime(LocalTime.MAX);
+        List<Diary> monthlyDiariesInfo = diaryRepository.findDiariesByCreatedAtBetweenAndUserId(startDateTime, endDateTime, user.getUserId());
+        List<DiaryContentDTO> monthlyDiaries = new ArrayList<>();
+        for(Diary diary : monthlyDiariesInfo) {
+            diaries.add(DiaryContentDTO.builder()
+                    .time(diary.getCreatedDate())
+                    .content(diary.getContent())
                     .build());
         }
 
         DailyAiLetterRequestDTO requestData = DailyAiLetterRequestDTO.builder()
                 .persona(persona)
-                .diarys(diaries)
-                .summary(summaries)
+                .diaries(diaries) // 요청일 하루의 일기 - AI 편지 생성용
+                .monthlyDiaries(monthlyDiaries) // 요청일 한달간의 일기 - 키워드 생성용
+                .summary(summaries) // 요청일 이전 2주간의 AI 응답
                 .build();
 
         try {
@@ -98,8 +109,6 @@ public class AIService {
 
     /**
      * 2. Letter 생성 API 호출
-     * @param requestData
-     * @return
      */
     public String requestLetter(Users user, DailyAiLetterRequestDTO requestData) {
         ResponseEntity<DailyLetterSummaryResponseDTO> response
@@ -121,8 +130,6 @@ public class AIService {
 
     /**
      * 3. 일기 요약 API 호출
-     * @param user
-     * @return
      */
     public String requestDiarySummary(Users user, DailyAiLetterRequestDTO requestData) {
         ResponseEntity<DailyLetterSummaryResponseDTO> response
@@ -141,97 +148,65 @@ public class AIService {
         return summary.getResult();
     }
 
-
     /**
-     * 4. 감정 분석 API 호출
-     * @param user
-     * @return
+     * 4. 월간 키워드 API 호출
      */
-    public DailyAiEmotionResponseDTO requestEmotion(Users user, DailyAiLetterRequestDTO requestData) {
-        ResponseEntity<DailyAiEmotionResponseDTO> response
-                = restTemplate.postForEntity(AI_EMOTION_URL, requestData, DailyAiEmotionResponseDTO.class);
+    public String requestKeywords(Users user, DailyAiLetterRequestDTO requestData) {
+        ResponseEntity<DailyLetterSummaryResponseDTO> response
+                = restTemplate.postForEntity(AI_KEYWORDS_URL, requestData, DailyLetterSummaryResponseDTO.class);
         if (response.getStatusCode() != HttpStatus.OK) {
-            log.error("---[SimSimSchedule] requestEmotion AI 응답 실패 userId = {}", user.getUserId());
+            log.error("---[SimSimSchedule] requestKeywords AI 응답 내용 없음 userId = {}", user.getUserId());
             return null;
         }
 
-        log.warn("---[SimSimSchedule] requestEmotion AI 응답 내용 {},  userId = {}", response.getBody(), user.getUserId());
-        DailyAiEmotionResponseDTO emotions = response.getBody();
-        if (emotions == null || emotions.getPositive() == null
-                || emotions.getNegative() == null || emotions.getNeutral() == null) {
-            log.error("---[SimSimSchedule] requestEmotion AI 응답 내용 없음 userId = {}", user.getUserId());
+        log.warn("---[SimSimSchedule] requestKeywords AI 응답 내용 {},  userId = {}", response.getBody(), user.getUserId());
+        DailyLetterSummaryResponseDTO keywords = response.getBody();
+        if (keywords == null || keywords.getResult().isEmpty()) {
+            log.error("---[SimSimSchedule] requestKeywords AI 응답 내용 없음 userId = {}", user.getUserId());
             return null;
         }
-        return emotions;
+        return keywords.getResult();
     }
 
 
     /**
      * AI 응답에 DB 저장 처리
-     * @param user
-     * @param targetDate
-     * @param startDateTime
-     * @param endDateTime
-     * @return
      */
     @Transactional
-    public DailyAiInfo requestToAI(Users user, LocalDate targetDate, LocalDateTime startDateTime, LocalDateTime endDateTime) {
-        // 예외 처리
-        List<Diary> targetDiaries = diaryRepository.findDiariesByCreatedAtBetweenAndUserId(startDateTime, endDateTime, user.getUserId());
-        if (targetDiaries.isEmpty()) {
-            log.info("---[SimSimSchedule] 해당 날짜({})에 작성한 일기가 없는 회원 userId = {}", targetDate, user.getUserId());
-            return null;
-        }
+    public AILetterResponseDTO requestToAI(Users user, LocalDate targetDate, List<Diary> targetDiaries) {
 
         // AI 요청 정보 생성
         DailyAiLetterRequestDTO requestData = generateRequestData(user, targetDate, targetDiaries);
 
         // AI 요청
         String letter = requestLetter(user, requestData); // AI_LETTER_URL 호출
-        DailyAiEmotionResponseDTO emotions = requestEmotion(user, requestData); //AI_EMONTION_URL 호출
         String summary = requestDiarySummary(user, requestData); // AI_SUMMARY_URL 호출
-        if (letter == null || emotions == null || summary == null) {
+        String keywords = requestKeywords(user, requestData); // AI_KEYWORDS_URL 호출
+        if (letter == null || keywords == null || summary == null) {
             throw new AIException(AIRESPONE_NOT_FOUND);
         }
 
-        // 모든 Diary의 isSendAble 상태를 false로 설정.
+        // 모든 Diary의 isSendAble 상태를 false로 설정
         diaryRepository.findAllByCreatedAtAndUserId(user.getUserId(), targetDate).forEach(diary -> diary.setIsSendAble(false));
-
-        DailyAiInfo saveData = dailyAiInfoRepository.save(DailyAiInfo.builder()
+        DailyAiInfo aiData = dailyAiInfoRepository.save(DailyAiInfo.builder()
                 .userId(user.getUserId())
                 .targetDate(targetDate)
                 .diarySummary(summary)
                 .replyContent(letter)
-                .happyCnt(emotions.getPositive().get(0))
-                .appreciationCnt(emotions.getPositive().get(1))
-                .loveCnt(emotions.getPositive().get(2))
-                .analyzePositive(emotions.getPositive().toString())
-                .analyzePositiveTotal(emotions.getPositive_total())
-                .tranquilityCnt(emotions.getNeutral().get(0))
-                .curiosityCnt(emotions.getNeutral().get(1))
-                .surpriseCnt(emotions.getNeutral().get(2))
-                .analyzeNeutral(emotions.getNeutral().toString())
-                .analyzeNeutralTotal(emotions.getNeutral_total())
-                .sadCnt(emotions.getNegative().get(0))
-                .angryCnt(emotions.getNegative().get(1))
-                .fearCnt(emotions.getNegative().get(2))
-                .analyzeNegative(emotions.getNegative().toString())
-                .analyzeNegativeTotal(emotions.getNegative_total())
                 .replyStatus("N")
                 .isFirst(false)
                 .build());
 
-        log.info("---[SimSimSchedule] 처리 완료 userId = {}", user.getUserId());
-        log.warn("---[SimSimStatus] targetDiaries[0].SendAble = {}", targetDiaries.getLast().getSendAble());
-        return saveData;
-    }
+        // Keywords 정보를 저장
+        MonthlyReport reportData = monthlyReportRepository.save(MonthlyReport.builder()
+                .userId(user.getUserId())
+                .targetDate(targetDate)
+                .keywordsData(keywords)
+                .monthlySummary(null) // TODO - 기능 추가 여부에 따라 제거 예정
+                .build());
 
-    private static List<Integer> convertStringToList(String str) {
-        if (str == null) {
-            return Arrays.asList(0, 0, 0);
-        }
-        return Arrays.stream(str.replaceAll("\\[|\\]", "").split(",\\s*"))
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
+        log.info("---[SimSimSchedule] 처리 완료 userId = {}", user.getUserId());
+        log.warn("---[SimSimStatus] targetDiaries[0].SendAble = {}", targetDiaries.getLast().getIsSendAble());
+        return new AILetterResponseDTO(aiData, reportData);
     }
 }
