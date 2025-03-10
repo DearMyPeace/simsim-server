ecb057c3b30d3894f66213b5dba438e6bc6481e7
YunjooCho <73283078+YunjooCho@users.noreply.github.com>
2025-03-10 20:38:25 +0900

    [Fix] Added Logger

--- src/main/java/com/project/simsim_server/dto/ai/fastapi/DailyAiKeywordReponseDTO.java
+++ src/main/java/com/project/simsim_server/dto/ai/fastapi/DailyAiKeywordReponseDTO.java
@@ -2,10 +2,12 @@ package com.project.simsim_server.dto.ai.fastapi;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.util.List;
 import java.util.Map;
 
+@ToString
 @Getter
 @NoArgsConstructor
 public class DailyAiKeywordReponseDTO {
