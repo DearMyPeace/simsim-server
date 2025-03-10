1bc0655d3809404b9f0fd8eed51790e30b1f6dc0
YunjooCho <73283078+YunjooCho@users.noreply.github.com>
2025-03-10 20:10:14 +0900

    Update DailyAiKeywordRequestDTO.java

--- src/main/java/com/project/simsim_server/dto/ai/fastapi/DailyAiKeywordRequestDTO.java
+++ src/main/java/com/project/simsim_server/dto/ai/fastapi/DailyAiKeywordRequestDTO.java
@@ -4,9 +4,11 @@ package com.project.simsim_server.dto.ai.fastapi;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.util.List;
 
+@ToString
 @Getter
 @NoArgsConstructor
 public class DailyAiKeywordRequestDTO {
