9809912a4b45689758993818284203ab4fa1558c
YunjooCho <73283078+YunjooCho@users.noreply.github.com>
2025-03-14 21:19:31 +0900

    [Feat] Implement the monthly report API

--- src/main/java/com/project/simsim_server/controller/ai/ReportController.java
+++ src/main/java/com/project/simsim_server/controller/ai/ReportController.java
@@ -2,12 +2,14 @@ package com.project.simsim_server.controller.ai;
 
 import com.project.simsim_server.config.auth.jwt.AuthenticationService;
 
+import com.project.simsim_server.dto.ai.client.AIMonthlyResponseDTO;
 import com.project.simsim_server.service.report.ReportService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Tag(name = "Report", description = "레포트 서비스")
 @RequiredArgsConstructor
@@ -17,4 +19,17 @@ public class ReportController {
 
     private final ReportService reportService;
     private final AuthenticationService authenticationService;
+
+    /**
+     * 월간 키워드 정보 조회
+     * @param targetDate year + month ex) 202503
+     * @return AIMonthlyResponseDTO 리스트 (rate, keyword, comment)
+     */
+    @GetMapping("/montly/{targetDate}")
+    public List<AIMonthlyResponseDTO> getAIMontlyReport(
+            @PathVariable String targetDate
+    ) {
+        Long userId = authenticationService.getUserIdFromAuthentication();
+        return reportService.findByuserIdAndTargetDate(userId, targetDate);
+    }
 }
--- src/main/java/com/project/simsim_server/service/report/ReportService.java
+++ src/main/java/com/project/simsim_server/service/report/ReportService.java
@@ -1,25 +1,18 @@
 package com.project.simsim_server.service.report;
 
 
-import com.project.simsim_server.domain.ai.DailyAiInfo;
-import com.project.simsim_server.domain.diary.Diary;
-import com.project.simsim_server.dto.ai.client.AnalyzeMaxInfoDTO;
-import com.project.simsim_server.dto.ai.client.WeekEmotionsResponseDTO;
-import com.project.simsim_server.dto.ai.client.WeekSummaryResponseDTO;
+import com.project.simsim_server.domain.ai.MonthlyReport;
+import com.project.simsim_server.dto.ai.client.AIMonthlyResponseDTO;
 import com.project.simsim_server.exception.ai.AIException;
-import com.project.simsim_server.repository.ai.DailyAiInfoRepository;
-import com.project.simsim_server.repository.diary.DiaryRepository;
+import com.project.simsim_server.repository.ai.MonthlyReportRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.Year;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
-import java.util.Optional;
 
 import static com.project.simsim_server.exception.ai.AIErrorCode.*;
 
@@ -29,6 +22,40 @@ import static com.project.simsim_server.exception.ai.AIErrorCode.*;
 @Service
 public class ReportService {
 
-    private final DiaryRepository diaryRepository;
-    private final DailyAiInfoRepository dailyAiInfoRepository;
+    private final MonthlyReportRepository monthlyReportRepository;
+
+    public List<AIMonthlyResponseDTO> findByuserIdAndTargetDate(
+            Long userId, String targetDate
+    ) {
+        if (isFuturDay(targetDate)) {
+            throw new AIException(AI_NOT_INVALID_DATE);
+        }
+        int targetYear = Integer.parseInt(targetDate.substring(0, 4));
+        int targetMonth = Integer.parseInt(targetDate.substring(4, 6));
+        log.info("---[SimSimInfo] 월간 레포트 targetYear : {}, targetMonth : {}", targetYear, targetMonth);
+
+        List<MonthlyReport> monthlyReportList
+                = monthlyReportRepository.findByIdAndTargetDate(userId, targetYear, targetMonth);
+        if (monthlyReportList.isEmpty()) {
+            throw new AIException(REPORT_NOT_FOUND);
+        }
+        String jsonData = monthlyReportList.getFirst().getKeywordsData();
+        List<AIMonthlyResponseDTO> responseList = AIMonthlyResponseDTO.convertFromJSONtoObject(jsonData);
+        if (responseList.isEmpty()) {
+            throw new AIException(REPORT_NOT_FOUND);
+        }
+
+        //TODO - 추후 삭제
+        for(AIMonthlyResponseDTO responseDTO : responseList) {
+            log.info("---[SimSimInfo] keyword={}, rate={}, comment={}",
+                    responseDTO.getKeyword(), responseDTO.getRate(), responseDTO.getComment());
+        }
+        return responseList;
+    }
+
+    private Boolean isFuturDay(String targetDate) {
+        YearMonth targetYearMonth = YearMonth.parse(targetDate, DateTimeFormatter.ofPattern("yyyyMM"));
+        YearMonth currentYearMonth = YearMonth.now();
+        return targetYearMonth.isAfter(currentYearMonth);
+    }
 }
