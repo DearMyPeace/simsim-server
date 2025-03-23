b18197596191382417fa2e102205b56a65afeb59
YunjooCho <73283078+YunjooCho@users.noreply.github.com>
2025-03-23 14:49:40 +0900

    [Fix] Modified DailyAiLetterRequestDTO class for target date

--- src/main/java/com/project/simsim_server/dto/ai/fastapi/DailyAiLetterRequestDTO.java
+++ src/main/java/com/project/simsim_server/dto/ai/fastapi/DailyAiLetterRequestDTO.java
@@ -4,18 +4,21 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Getter
 @NoArgsConstructor
 public class DailyAiLetterRequestDTO {
+    private LocalDate targetDate;
     private List<DiaryContentDTO> diarys;
     private List<DiaryContentDTO> monthlyDiaries;
     private String persona;
     private List<DiarySummaryDTO> summary;
 
     @Builder
-    public DailyAiLetterRequestDTO(List<DiaryContentDTO> diarys, List<DiaryContentDTO> monthlyDiaries, String persona, List<DiarySummaryDTO> summary) {
+    public DailyAiLetterRequestDTO(LocalDate targetDate, List<DiaryContentDTO> diarys, List<DiaryContentDTO> monthlyDiaries, String persona, List<DiarySummaryDTO> summary) {
+        this.targetDate = targetDate;
         this.diarys = diarys;
         this.monthlyDiaries = monthlyDiaries;
         this.persona = persona;
